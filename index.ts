11fdd89f630941f704effc941ed5de38589f8de5
Deepesh kumar <dr.deepeshgiri@gmail.com>
2024-12-14 17:23:38 +0530

    updated peer dependency from 10 to latest
    it was throwing error and conflicting with current angular versions.

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 /**
 Author Name  :  WeblineIndia  |  https://www.weblineindia.com/
-
+Modified by: deepesh_giri
 For more such software development components and code libraries, visit us at
 https://www.weblineindia.com/software-development-resources.html 
 
